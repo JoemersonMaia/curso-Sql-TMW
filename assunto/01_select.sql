b5691a3b674ca5f2f223024567007c7fde6af605
joemerson_maia <seuemail@exemplo.com>
2026-09-10 19:46:47 -0300

    resolucao_da_perca_de_dados

--- assunto/01_select.sql
+++
@@ -1,3 +0,0 @@
-SELECT *
-FROM clientes
-LIMIT 10
--- assunto/05_select_col.sql
+++
@@ -1,7 +0,0 @@
-SELECT  IdTransacao,
-        DtCriacao,
-        datetime(substr(DtCriacao,1,19)) AS DtCriacaoNova,
-        strftime('%w', datetime(substr(DtCriacao,1,19))) AS DiaSemana
-FROM transacoes
-
-WHERE DiaSemana = '6' or DiaSemana = '5'
