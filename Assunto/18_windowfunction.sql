c29f4a958a6ca2e76ae69e141ab0aebfd0e5998d
joemerson_maia <seuemail@exemplo.com>
2026-09-18 19:30:03 -0300

    window_function

---
+++ Assunto/18_windowfunction.sql
@@ -0,0 +1,17 @@
+WITH tb_sumario_dias AS(
+    SELECT 
+    substr(DtCriacao, 1, 10) AS dtDia,
+    count(IdTransacao) AS qteTransacoes
+
+    FROM transacoes
+
+    WHERE DtCriacao >= '2025-08-25'
+    AND DtCriacao < '2025-08-30'
+
+    GROUP BY dtDia
+)
+
+SELECT 
+*,
+sum(qteTransacoes)  OVER (ORDER BY dtDia) ASqteTransacoesCum
+FROM tb_sumario_dias
--- Assunto/20_windowfunction.sql
+++ Assunto/20_windowfunction.sql
@@ -1,8 +1,37 @@
-SELECT 
-IdCLiente, 
-substr(DtCriacao, 1, 10) as dtDia
-FROM transacoes
+WITH cliente_dia AS( 
+    SELECT 
+    DISTINCT IdCLiente, 
+    substr(DtCriacao, 1, 10) as dtDia
+    FROM transacoes
 
-WHERE substr(DtCriacao, 1 ,4) = '2025'
+    WHERE substr(DtCriacao, 1 ,4) = '2025'
 
-ORDER BY IdCLiente, DtDia
+    ORDER BY IdCLiente, DtDia
+),
+
+tab_lag AS (
+    SELECT 
+    *,
+    lag(dtDia) OVER (PARTITION BY IdCLiente ORDER BY dtDia) AS lagdia
+
+    FROM cliente_dia
+),
+
+tb_diff_dt AS   (
+    SELECT 
+    *,
+    julianday(dtDia) - julianday(lagDia) as DtDiff
+
+    FROM tab_lag
+),
+
+
+avg_cliente AS (
+
+    SELECT IdCLiente, avg(DtDiff) AS avgDia
+
+    FROM tb_diff_dt
+    GROUP BY IdCLiente
+)
+
+SELECT avg(avgDia) FROM avg_cliente
